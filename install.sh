e2d0307ac47fb3af218dde0cee820dc5e4bc072e
Deva Midhun <dev@deva.is-cool.dev>
2023-08-11 17:31:53 +0530

    pub key was not working regenerated it

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo "Getting Public Key...."
-wget https://pkgs.deva.is-cool.dev/repo.pub -q -O /data/data/com.termux/files/usr/etc/apt/trusted.gpg.d/repo.pub
+wget https://pkgs.deva.is-cool.dev/repo.gpg -q -O /data/data/com.termux/files/usr/etc/apt/trusted.gpg.d/repo.gpg
 echo "Adding repo list....."
 wget https://pkgs.deva.is-cool.dev/deva.list -q -O /data/data/com.termux/files/usr/etc/apt/sources.list.d/deva.list
 echo "Updating package lists..."
